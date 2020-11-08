d6f8cd667270319d03d8fea6564038308052dd8d
Helbert Nunes <helbertnunes97@gmail.com>
2020-11-08 16:51:21 -0300

    Adição banco inmemory

--- TesteNava/src/TesteNava.Data/Context/TesteNavaDbContext.cs
+++ TesteNava/src/TesteNava.Data/Context/TesteNavaDbContext.cs
@@ -14,6 +14,11 @@ namespace TesteNava.Data.Context
         public DbSet<SaleItem> SaleItems { get; set; }
         public DbSet<Seller> Sellers { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("InMemoryProvider");
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(TesteNavaDbContext).Assembly);
