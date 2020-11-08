892451cd8f0afb003ff3ca8afaff011c43d17843
Helbert Nunes <helbertnunes97@gmail.com>
2020-11-08 17:32:33 -0300

    Injeção de dependencia no startup

--- TesteNava/src/TesteNava.API/Startup.cs
+++ TesteNava/src/TesteNava.API/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TesteNava.Data.Context;
+using TesteNava.Data.Repository;
+using TesteNava.Domain.Interfaces;
 
 namespace TesteNava.API
 {
@@ -26,6 +29,11 @@ namespace TesteNava.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<TesteNavaDbContext>();
+            services.AddScoped<ISaleItemRepository, SaleItemRepository>();
+            services.AddScoped<ISaleRepository, SaleRepository>();
+            services.AddScoped<ISellerRepository, SellerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
